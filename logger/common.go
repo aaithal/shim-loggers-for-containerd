e668ef77200afb813fc91e13c18917ac298143f9
Anuj Singh <singholt@amazon.com>
2025-04-09 09:47:22 -0700

    add debug logs to track log messages across shim-logger, fluentd, fluent-logger-golang

--- logger/common.go
+++ logger/common.go
@@ -510,6 +510,12 @@ func (l *Logger) sendLogMsgToDest(
 	if isPartialMsg {
 		message.PLogMetaData = &types.PartialLogMetaData{ID: partialID, Ordinal: partialOrdinal, Last: isLastPartial}
 	}
+
+	// Log message sent from shim-logger -> fluentd
+	debug.SendEventsToLog(l.Info.ContainerID,
+		fmt.Sprintf("[Pipe %s] Sending message to fluentd: %+v", source, message),
+		debug.DEBUG, 0)
+
 	err := l.Log(message)
 	if err != nil {
 		// If we return a non-empty error here, it will cause the goroutine exits. As a result, it won't consume logs from stdout/stderr
